dd50e06df484da62a83bc16e825416fb1544b0bf
Hugo Montana <benzlbenny2k@gmail.com>
2023-02-14 22:05:07 +0200

    Beam

---
+++ Assets/Script/TowerBuilder.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerBuilder : MonoBehaviour
+{
+    [SerializeField] private int _levelCount;
+    [SerializeField] private GameObject _beam;
+
+    private void Start()
+    {
+        Build();
+    }
+
+    private void Build()
+    {
+        GameObject beam = Instantiate(_beam, transform);
+        beam.transform.localScale = new Vector3(1 ,_levelCount / 2f, 1);
+    }
+}

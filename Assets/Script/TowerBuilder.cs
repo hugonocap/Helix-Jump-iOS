34444d227a83175c93eb033854d699bb25be8913
Hugo Montana <benzlbenny2k@gmail.com>
2023-02-15 19:38:23 +0200

    builder script updates

--- Assets/Script/TowerBuilder.cs
+++ Assets/Script/TowerBuilder.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class TowerBuilder : MonoBehaviour
 {
     [SerializeField] private int _levelCount;
+    [SerializeField] private float _additionalScale;
     [SerializeField] private GameObject _beam;
     [SerializeField] private SpawnPlatform _spawnPlatform;
     [SerializeField] private Platform[] _platform;
     [SerializeField] private FinishPlatform _finishPlatform;
+    
+
+    private float _startAndFinishAdditionalScale = 0.5f;
+    public float BeamScaleY => _levelCount / 2f + _startAndFinishAdditionalScale + _additionalScale /2f;
 
     private void Start()
     {
@@ -18,6 +23,24 @@ public class TowerBuilder : MonoBehaviour
     private void Build()
     {
         GameObject beam = Instantiate(_beam, transform);
-        beam.transform.localScale = new Vector3(5  ,_levelCount / 2f, 5);
+        beam.transform.localScale = new Vector3(1 ,BeamScaleY , 1);
+
+        Vector3 spawnPosition = beam.transform.position;
+        spawnPosition.y += beam.transform.localScale.y - _additionalScale;
+
+        SpawnPlatform(_spawnPlatform, ref spawnPosition, beam.transform);
+
+        for(int i = 0; i < _levelCount; i++)
+        {
+            SpawnPlatform(_platform[Random.Range(0, _platform.Length)], ref spawnPosition, beam.transform);
+        }
+
+        SpawnPlatform(_finishPlatform, ref spawnPosition, beam.transform);
+    }
+
+    private void SpawnPlatform(Platform platform, ref Vector3 spawnPosition, Transform parent)
+    {
+        Instantiate(platform, spawnPosition, Quaternion.Euler(0, Random.Range(0, 360), 0), parent);
+        spawnPosition.y -= 1;
     }
 }
